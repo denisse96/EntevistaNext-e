b49048fcbb9e14c86de2366e79f26daeddd16ac7
denisse96 <denissedam@gmail.com>
2020-04-06 15:06:45 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,60 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import axios from "axios";
 
-const IndexPage = () => (
+
+const IndexPage = () => {
+  let APIKey = "05999112ed464d9b9cccbaa799a9405f"
+  const [newsData, setNewsData] = useState({})
+  
+  async function getNews(country){
+    let param = "country=" + country + "&"
+    let response = await axios.get("https://newsapi.org/v2/top-headlines?"+param+ "apiKey=" + APIKey );
+    console.log({response})
+    setNewsData({artiles : response.data.artiles})
+  }
+
+
+
+  useEffect(() => {
+    getNews("us")
+
+   
+
+  }, []);
+
+useEffect(() =>{
+console.log({newsData})
+}, [newsData])
+
+function handleCountryChange(event){
+  console.log(event)
+
+}
+
+return (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <input id="country"  type= "text"></input>
+
+    <ol>
+      {newsData.map(data => (
+        <li >{data.title} 
+        {data.description}
+        <image >{data.url}</image>
+        </li>
+      ))}
+    </ol>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
+
 )
+
+}
+
 
 export default IndexPage
