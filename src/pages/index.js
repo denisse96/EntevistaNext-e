f4615099b4629906d6a09373f2379b3f7781f25d
denisse96 <denissedam@gmail.com>
2020-04-07 00:59:43 -0500

    added filter for country

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,33 @@ import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 import SEO from "../components/seo"
 import axios from "axios";
-
+import { useFormik } from 'formik';
 
 const IndexPage = () => {
+  const formik = useFormik({
+    initialValues: {
+     country:"us", 
+     keyword:""
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+
   let APIKey = "05999112ed464d9b9cccbaa799a9405f"
-  const [newsData, setNewsData] = useState({})
-  
+  const [newsData, setNewsData] = useState( {articles: []} )
+
+  console.log({newsData}, formik.values)
+ 
   async function getNews(country){
     let param = "country=" + country + "&"
     let response = await axios.get("https://newsapi.org/v2/top-headlines?"+param+ "apiKey=" + APIKey );
     console.log({response})
-    setNewsData({artiles : response.data.artiles})
+    setNewsData({articles : response.data.articles})
   }
 
 
@@ -23,32 +36,41 @@ const IndexPage = () => {
   useEffect(() => {
     getNews("us")
 
-   
-
   }, []);
 
-useEffect(() =>{
-console.log({newsData})
-}, [newsData])
+  useEffect(() => {
+    getNews(formik.values.country)
 
-function handleCountryChange(event){
-  console.log(event)
+  }, [formik.values.country]);
 
-}
+
+
+
 
 return (
   <Layout>
     <SEO title="Home" />
-    <input id="country"  type= "text"></input>
+  
+    <label htmlFor="keyword">KeyWord</label>
+    <input type="text" id="keyword" value={formik.values.keyword} onChange={formik.handleChange}></input>
+    <select name="country" id="country" value = {formik.values.country} onChange={formik.handleChange}>
+      <option value ="ae">ae</option>
+      <option value = "ar">ar</option>
+      <option value = "at">at</option>
+      <option value = "au">au</option>
+      <option value = "us">us</option>
+    </select>
 
-    <ol>
-      {newsData.map(data => (
-        <li >{data.title} 
-        {data.description}
-        <image >{data.url}</image>
+    <ul>
+      {newsData.articles.map((item, index) => (
+        <li key={index}>
+          <h1>{item.title}</h1>
+          <h4>{item.description}</h4>
+          <img src={item.urlToImage} alt ={item.title}></img>
         </li>
       ))}
-    </ol>
+    </ul>
+   
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 
